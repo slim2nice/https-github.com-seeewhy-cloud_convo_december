dacd76c6b344c6d73b96259d6ddef9ea6119a7fe
seeewhy <85683554+seeewhy@users.noreply.github.com>
2023-12-02 18:21:44 +0100

    fix: added keys

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
+
+
+
 provider "aws"
     region = "us-east-2"
+access_key = "gdtuifdu8973878"
+secret_key = "8676766*&^$#$^2jdsdgg"
 resource "aws_ec2" "first-instance" {
 cidr_blocks = "10.0.0.0/0"
 
